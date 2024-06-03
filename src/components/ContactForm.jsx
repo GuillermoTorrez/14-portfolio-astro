3eec990f41277fb3cfb992407001b2cdb94fbffb
GuillermoTorrez <guillermo.torrez@gmail.com>
2024-06-02 22:44:26 -0400

    Update Imagen Issue

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,20 +1,18 @@
-
-export function ContactForm  () {
-  
+export function ContactForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const form = event.currentTarget;
     const formData = new FormData(form);
 
-    const name = formData.get("name") 
-    const email = formData.get("email") 
-    const phone = formData.get("phone") 
-    const message = formData.get("message") 
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const phone = formData.get("phone");
+    const message = formData.get("message");
 
     const dataToSubmit = { name, email, phone, message };
 
-    console.log(dataToSubmit)
+    console.log(dataToSubmit);
 
     try {
       const response = await fetch(import.meta.env.API_URL_TWI, {
@@ -36,63 +34,80 @@ export function ContactForm  () {
       alert("Error: " + error.message);
       console.log(error);
     }
-    
   };
 
   return (
     <>
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <form onSubmit={handleSubmit} id="contact-form" method="POST">
-        <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">
-            Phone Number
-          </label>
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            rows={4}
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-            required
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
-        >
-          Send Message
-        </button>
-      </form>
-    </div>
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <form onSubmit={handleSubmit} id="contact-form" method="POST">
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Phone Number
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </>
   );
-};
+}
